1c1ba8b3fe539212d9c78fb085e12356e9cde8aa
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2026-01-13 19:12:12 +0100

    fix: failed build

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -22,7 +22,7 @@ Object.defineProperty(window, 'matchMedia', {
   })),
 });
 
-global.ResizeObserver = vi.fn().mockImplementation(() => ({
+globalThis.ResizeObserver = vi.fn().mockImplementation(() => ({
   observe: vi.fn(),
   unobserve: vi.fn(),
   disconnect: vi.fn(),
